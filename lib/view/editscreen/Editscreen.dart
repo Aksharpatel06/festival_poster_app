8a2ce94d6fdd822594a4d83a3ff8acf20877b837
Aksharpatel06 <aksharpatel030306@gmail.com>
2024-03-30 23:52:08 +0530

    30-03-2024

--- lib/view/editscreen/Editscreen.dart
+++ lib/view/editscreen/Editscreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../../utils/festivallist.dart';
+import '../../utils/global_variable.dart';
 
 class Edit_screen extends StatefulWidget {
   const Edit_screen({super.key});
@@ -14,7 +16,123 @@ class _Edit_screenState extends State<Edit_screen> {
       appBar: AppBar(
         iconTheme: IconThemeData(color: Colors.white),
         backgroundColor: Color(0xff1c2438),
-        title: Text('Edit',style: TextStyle(color: Colors.white),),
+        title: Text(
+          'Edit',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                      height: 300,
+                      width: 300,
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey,
+                            blurRadius: 0.5,
+                            spreadRadius: 1,
+                          )
+                        ],
+                        color: Colors.black,
+                      ),
+                      child: Image.asset(
+                        festivalList[postviewIndex]['Images'],
+                        fit: BoxFit.cover,
+                      )),
+                ],
+              ),
+            ),
+          ),
+          IndexedStack(
+            index: 4,
+            children: [
+            ],
+          ),
+          Container(
+            height: 70,
+            decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black,
+                    blurRadius: 5,
+                    spreadRadius: 2,
+                  )
+                ],
+                color: Color(0xff1c2438),
+                borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(20),
+                    topLeft: Radius.circular(20))),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                InkWell(
+                  onTap: () {},
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.draw,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'Canvas',
+                        style: TextStyle(color: Colors.white),
+                      )
+                    ],
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.text_fields,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      'Text',
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.share,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      'Share',
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.save_alt,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      'Save',
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
